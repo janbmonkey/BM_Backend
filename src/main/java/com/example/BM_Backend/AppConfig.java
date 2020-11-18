5747adfb06853bfa0f7cadde079ba163d63cd8bf
IXTO\qnguyen <qnguyen@ixto.de>
2020-11-18 19:38:09 +0100

    update cors config

--- src/main/java/com/example/BM_Backend/AppConfig.java
+++ src/main/java/com/example/BM_Backend/AppConfig.java
@@ -14,7 +14,8 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000");
+        .allowedOrigins("http://localhost:3000")
+        .allowedMethods("GET", "PUT", "DELETE");
     }
 
     @Bean
